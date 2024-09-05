d86fca308e0edd146df75d6039ba751b9663aa94
Taehoon1 <taehoonoh04@gmail.com>
2024-09-05 17:54:10 +0000

    not done

--- Program3.py
+++ Program3.py
@@ -1,3 +1,11 @@
+myNewInt= "180"
+print(int(myNewInt)/6)
+num1= input("Enter a number")
+print(int(num1)+int(num1))
+thing= "home"
+print(f"The result is {thing}")
+
+
 ## Problem 1 ##
 #Fix the following code so that its output matches
 # Sample output:
@@ -22,12 +30,13 @@ level3 = "semiprofessional"
 lower = 2000
 upper = 3000
 
-print("my name is ", name, " , I am ", age, "years old")
-print("my skills are")
-print("- ", skill1, " (", level1, ")")
-print("- ", skill2, " (", level2, ")")
-print("- ", skill3, " (", level3, " )")
-print("I am looking for a job with a salary of", lower, "-", upper, "dollars per month")
+print("my name is", name+",", "I am",age,"years old")
+print("\n my skills are")
+print(" -",skill1,"("+level1+")")
+print(" -",skill2,"("+level2+")")
+print(" -",skill3,"("+level3+")")
+print("\n I am looking for a job with a salary of",lower,"-",upper,"dollars per month")
+
 
 
 
@@ -51,3 +60,10 @@ print("I am looking for a job with a salary of", lower, "-", upper, "dollars per
 x = input("X val: ")
 y = input("Y val: ")
 
+print("X val:", x)
+print("Y val:", y)
+print(" ")
+print(x,"+",y,"=",int(int(x)+int(y)))
+print(x,"-",y,"=",int(int(x)-int(y)))
+print(x,"*",y,"=",int(int(x)*int(y)))
+print(x,"/",y,"=",int(int(x)/int(y)))
